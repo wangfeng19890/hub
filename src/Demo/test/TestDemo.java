e74082c6bb6f4d8ed672df62a6916f8cf113e137
weipeng <weipeng@weipeng-PC>
2020-02-08 23:48:55 +0800

    add

--- src/Demo/test/TestDemo.java
+++ src/Demo/test/TestDemo.java
@@ -5,7 +5,7 @@ public class TestDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(123);
-		System.out.println(123456);
+		System.out.println(1234567890);
 	}
 
 }
